c7ff5cbb9973ab55838d6dcc6fa3f6a6016f0824
NigunSanjai <radha.nigun@gmail.com>
2023-06-26 12:35:46 +0530

    f

--- frontend/src/app/auth.service.ts
+++ frontend/src/app/auth.service.ts
@@ -8,7 +8,7 @@ import { SocialUser } from '@abacritt/angularx-social-login';
 })
 export class AuthService {
   notloggedIn = true;
-  private BASE_URL = 'https://7534-103-114-208-197.in.ngrok.io';
+  private BASE_URL = 'https://67d2-103-114-208-197.in.ngrok.io';
 
   constructor(private http: HttpClient) {}
 
